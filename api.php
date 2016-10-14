a97c4afb70f050e5b2f3efd9698ed95138619844
wbentley15 <wbentley@hitchnyc.com>
2016-10-14 12:39:28 -0500

    ready to go

--- api.php
+++ api.php
@@ -21,6 +21,15 @@ function get_app_by_id($id)
     case 4:
       $app_info = array("os_release_name" => "Libety", "app_price" => "Free", "rpc_version" => "12.0");
       break;
+    case 5:
+      $app_info = array("os_release_name" => "Mitaka", "app_price" => "Free", "rpc_version" => "13.0");
+      break;
+    case 6:
+      $app_info = array("os_release_name" => "Newton", "app_price" => "Free", "rpc_version" => "14.0");
+      break;
+    case 7:
+      $app_info = array("os_release_name" => "Ocata", "app_price" => "Free", "rpc_version" => "15.0");
+      break;
   }
 
   return $app_info;
@@ -30,14 +39,14 @@ function get_app_list()
 {
   //normally this info would be pulled from a database.
   //build JSON array
-  $app_list = array(array("id" => 1, "name" => "Icehouse"), array("id" => 2, "name" => "Juno"), array("id" => 3, "name" => "Kilo"), array("id" => 4, "name" => "Liberty")); 
+  $app_list = array(array("id" => 1, "name" => "Icehouse"), array("id" => 2, "name" => "Juno"), array("id" => 3, "name" => "Kilo"), array("id" => 4, "name" => "Liberty"), array("id" => 5, "name" => "Mitaka"), array("id" => 6, "name" => "Newton"), array("id" => 7, "name" => "Ocata")); 
 
   return $app_list;
 }
 
 $possible_url = array("get_app_list", "get_app");
 
-$value = "An error has occurred";
+$value = "An error has occurred";	
 
 if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url))
 {
